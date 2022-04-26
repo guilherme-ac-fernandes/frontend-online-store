1ee90021ca3f5a15d507525ac1e935599054f121
Yuri Mascarenhas <dev.yurimascarenhas@gmail.com>
2022-04-26 16:46:21 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>APP</h1>
+
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={ Home } />
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;

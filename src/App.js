71ef2025c6f9766cbbd56bb7105dd9c077113fe8
Vinicius de Medeiros <vmeedeiros@gmail.com>
2022-04-27 15:38:21 -0300

    Requisito 6

--- src/App.js
+++ src/App.js
@@ -3,20 +3,62 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ShoppingCart from './Pages/ShoppingCart';
 import Home from './Pages/Home';
+import * as api from './services/api';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor() {
+    super();
 
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Home } />
-          <Route path="/shopping-cart" render={ () => <ShoppingCart /> } />
-        </Switch>
-      </BrowserRouter>
+    this.state = {
+      categoriaList: [],
+      categoriaId: '',
+      productList: [],
+      filtrar: false,
+      query: '',
+    };
+  }
 
-    </div>
-  );
+  async componentDidMount() {
+    const categorias = await api.getCategories();
+    this.setState({ categoriaList: categorias });
+  }
+
+  handleClick = async () => {
+    const { categoriaId, query } = this.state;
+    const listaProdutos = await api.getProductsFromCategoryAndQuery(categoriaId, query);
+    this.setState({ productList: listaProdutos.results, filtrar: true });
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.handleClick());
+  }
+
+  render() {
+    const { categoriaList, productList, filtrar } = this.state;
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={ () => (<Home
+                categoriaList={ categoriaList }
+                productList={ productList }
+                handleChange={ this.handleChange }
+                handleClick={ this.handleClick }
+                filtrar={ filtrar }
+              />) }
+            />
+            <Route path="/shopping-cart" render={ () => <ShoppingCart /> } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;

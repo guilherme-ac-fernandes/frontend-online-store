16b1b13fe1a59e0e475644f7984264b670699ba2
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-04-28 16:33:06 -0300

    Requisito 11 - em andamento

--- src/Pages/PageItem.js
+++ src/Pages/PageItem.js
@@ -1,12 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ShopButton from '../components/ShopButton';
+import FormItem from '../components/FormItem';
+import * as api from '../services/api';
 
 class PageItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      stars: 0,
+      avaliation: '',
+      comments: [],
+      product: [],
+    };
+  }
+
+  componentDidMount() {
+    // Baseada na resolução no exercício do Guilherme (link: https://github.com/guilherme-ac-fernandes/trybe-exercicios/blob/main/02-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/exercise-01/src/App.js)
+    const { match: { params: { id } } } = this.props;
+    const product = api.getProductsFromId(id);
+    this.setState({ product });
+
+    const storage = localStorage.getItem('comments');
+    if (storage !== null) {
+      this.setState({
+        comments: JSON.parse(storage),
+      });
+    }
+  }
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleClick = () => {
+    const { match: { params: { id } } } = this.props;
+    const { email, stars, avaliation } = this.state;
+    const object = { id, email, stars, avaliation };
+    console.log(object);
+    this.setState((prev) => ({
+      comments: [...prev.comments, object],
+    }), () => {
+      const { comments } = this.state;
+      localStorage.setItem('comments', JSON.stringify(comments));
+    });
+  };
+
   render() {
     const { match: { params: { id } }, productList, handleFavorites } = this.props;
-    const result = productList.find((elemento) => elemento.id === id);
-    const { title, thumbnail, price, attributes } = result;
+    const { comments, product } = this.state;
+    console.log(product);
+    const commentsFilter = comments.filter((item) => item.id === id);
+    const { title, thumbnail, price, attributes } = product;
     return (
       <div>
         <ShopButton />
@@ -16,7 +66,7 @@ class PageItem extends React.Component {
           <p>{price}</p>
           <button
             type="button"
-            onClick={ () => handleFavorites(result) }
+            onClick={ () => handleFavorites(product) }
             data-testid="product-detail-add-to-cart"
           >
             Adicionar ao carrinho
@@ -28,6 +78,17 @@ class PageItem extends React.Component {
               ))
             }
           </ul>
+          <FormItem
+            handleChange={ this.handleChange }
+            handleClick={ this.handleClick }
+          />
+          {commentsFilter.length > 0 && commentsFilter.map((item, index) => (
+            <div key={ index }>
+              <p>{ item.email }</p>
+              <p>{ item.stars }</p>
+              <p>{ item.avaliation }</p>
+            </div>
+          ))}
         </div>
       </div>
     );
